b42679ed4257828e618414dfc5e8a7b49728a9c8
Armin Ronacher <armin.ronacher@active-4.com>
2017-04-26 12:37:42 +0200

    Added download example

---
+++ examples/download.rs
@@ -0,0 +1,29 @@
+extern crate indicatif;
+
+use std::thread;
+use std::cmp::min;
+use std::time::Duration;
+use std::borrow::Cow;
+
+use indicatif::{ProgressBar, ProgressStyle};
+
+fn main() {
+    let mut downloaded = 0;
+    let total_size = 231231231;
+
+    let pb = ProgressBar::new(total_size);
+    let mut sty = ProgressStyle::default();
+    sty.bar_template = Cow::Borrowed("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes}");
+    sty.progress_chars = "#>-".chars().collect();
+    pb.set_style(sty);
+    pb.enable_spinner();
+
+    while downloaded < total_size {
+        let new = min(downloaded + 223211, total_size);
+        downloaded = new;
+        pb.set_position(new);
+        thread::sleep(Duration::from_millis(12));
+    }
+
+    pb.finish_with_message("downloaded");
+}
