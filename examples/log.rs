59f406b6e5d4f7035d9d9006eb7996c2953bc241
kpcyrd <git@rxv.cc>
2018-03-31 23:54:27 +0200

    Allow logging above the progress bar

---
+++ examples/log.rs
@@ -0,0 +1,16 @@
+extern crate indicatif;
+
+use std::thread;
+use std::time::Duration;
+
+use indicatif::ProgressBar;
+
+fn main() {
+    let pb = ProgressBar::new(100);
+    for i in 0..100 {
+        thread::sleep(Duration::from_millis(250));
+        pb.println(format!("[+] finished #{}", i));
+        pb.inc(1);
+    }
+    pb.finish_with_message("done");
+}
