fbd591601b1782bfa91f87d1160610c8034a0cca
mibac138 <5672750+mibac138@users.noreply.github.com>
2020-07-13 19:23:05 +0200

    Minor improvement of ProgressStyle::template docs

--- src/style.rs
+++ src/style.rs
@@ -110,6 +110,8 @@ impl ProgressStyle {
     }
 
     /// Sets the template string for the progress bar.
+    ///
+    /// List of keys is available at crate root docs.
     pub fn template(mut self, s: &str) -> ProgressStyle {
         self.template = Cow::Owned(s.into());
         self
