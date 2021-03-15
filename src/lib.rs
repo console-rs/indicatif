94acc270a7e679953ae219172e570e6f732a51b3
mibac138 <5672750+mibac138@users.noreply.github.com>
2021-03-15 13:48:32 +0100

    Add more implementations for ProgressBarIter

--- src/lib.rs
+++ src/lib.rs
@@ -212,9 +212,7 @@ mod utils;
 
 pub use crate::format::{BinaryBytes, DecimalBytes, FormattedDuration, HumanBytes, HumanDuration};
 pub use crate::iter::{ProgressBarIter, ProgressIterator};
-pub use crate::progress::{
-    MultiProgress, ProgressBar, ProgressDrawTarget, WeakProgressBar,
-};
+pub use crate::progress::{MultiProgress, ProgressBar, ProgressDrawTarget, WeakProgressBar};
 pub use crate::style::ProgressStyle;
 
 #[cfg(feature = "rayon")]
