aa3256c84a698a9c3893e0100b2099783a914a30
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-03-21 21:28:29 +0100

    Reorder code in state module

--- src/state.rs
+++ src/state.rs
@@ -8,254 +8,6 @@ use crate::style::{ProgressFinish, ProgressStyle};
 use crate::utils::{duration_to_secs, secs_to_duration, Estimate};
 use console::Term;
 
-/// The drawn state of an element.
-#[derive(Clone, Debug)]
-pub(crate) struct ProgressDrawState {
-    /// The lines to print (can contain ANSI codes)
-    pub lines: Vec<String>,
-    /// The number of lines that shouldn't be reaped by the next tick.
-    pub orphan_lines: usize,
-    /// True if the bar no longer needs drawing.
-    pub finished: bool,
-    /// True if drawing should be forced.
-    pub force_draw: bool,
-    /// True if we should move the cursor up when possible instead of clearing lines.
-    pub move_cursor: bool,
-}
-
-#[derive(Debug)]
-pub(crate) enum Status {
-    InProgress,
-    DoneVisible,
-    DoneHidden,
-}
-
-pub(crate) enum ProgressDrawTargetKind {
-    Term {
-        term: Term,
-        last_state: Option<ProgressDrawState>,
-        rate: Option<Duration>,
-        last_draw: Option<Instant>,
-    },
-    Remote {
-        state: Arc<RwLock<MultiProgressState>>,
-        idx: usize,
-        chan: Mutex<Sender<(usize, ProgressDrawState)>>,
-    },
-    Hidden,
-}
-
-/// Target for draw operations
-///
-/// This tells a progress bar or a multi progress object where to paint to.
-/// The draw target is a stateful wrapper over a drawing destination and
-/// internally optimizes how often the state is painted to the output
-/// device.
-pub struct ProgressDrawTarget {
-    pub(crate) kind: ProgressDrawTargetKind,
-}
-
-impl ProgressDrawTarget {
-    /// Draw to a buffered stdout terminal at a max of 15 times a second.
-    ///
-    /// For more information see `ProgressDrawTarget::to_term`.
-    pub fn stdout() -> ProgressDrawTarget {
-        ProgressDrawTarget::term(Term::buffered_stdout(), 15)
-    }
-
-    /// Draw to a buffered stderr terminal at a max of 15 times a second.
-    ///
-    /// This is the default draw target for progress bars.  For more
-    /// information see `ProgressDrawTarget::to_term`.
-    pub fn stderr() -> ProgressDrawTarget {
-        ProgressDrawTarget::term(Term::buffered_stderr(), 15)
-    }
-
-    /// Draw to a buffered stdout terminal at a max of `refresh_rate` times a second.
-    ///
-    /// For more information see `ProgressDrawTarget::to_term`.
-    pub fn stdout_with_hz(refresh_rate: u64) -> ProgressDrawTarget {
-        ProgressDrawTarget::term(Term::buffered_stdout(), refresh_rate)
-    }
-
-    /// Draw to a buffered stderr terminal at a max of `refresh_rate` times a second.
-    ///
-    /// For more information see `ProgressDrawTarget::to_term`.
-    pub fn stderr_with_hz(refresh_rate: u64) -> ProgressDrawTarget {
-        ProgressDrawTarget::term(Term::buffered_stderr(), refresh_rate)
-    }
-
-    /// Draw to a buffered stdout terminal without max framerate.
-    ///
-    /// This is useful when data is known to come in very slowly and
-    /// not rendering some updates would be a problem (for instance
-    /// when messages are used extensively).
-    ///
-    /// For more information see `ProgressDrawTarget::to_term`.
-    pub fn stdout_nohz() -> ProgressDrawTarget {
-        ProgressDrawTarget::term(Term::buffered_stdout(), None)
-    }
-
-    /// Draw to a buffered stderr terminal without max framerate.
-    ///
-    /// This is useful when data is known to come in very slowly and
-    /// not rendering some updates would be a problem (for instance
-    /// when messages are used extensively).
-    ///
-    /// For more information see `ProgressDrawTarget::to_term`.
-    pub fn stderr_nohz() -> ProgressDrawTarget {
-        ProgressDrawTarget::term(Term::buffered_stderr(), None)
-    }
-
-    /// Draw to a terminal, optionally with a specific refresh rate.
-    ///
-    /// Progress bars are by default drawn to terminals however if the
-    /// terminal is not user attended the entire progress bar will be
-    /// hidden.  This is done so that piping to a file will not produce
-    /// useless escape codes in that file.
-    ///
-    /// Will panic if refresh_rate is `Some(0)`. To disable rate limiting use `None` instead.
-    #[allow(clippy::wrong_self_convention)]
-    #[deprecated(since = "0.16.0", note = "Use `ProgressDrawTarget::term` instead")]
-    pub fn to_term(term: Term, refresh_rate: impl Into<Option<u64>>) -> ProgressDrawTarget {
-        ProgressDrawTarget::term(term, refresh_rate)
-    }
-
-    /// Draw to a terminal, optionally with a specific refresh rate.
-    ///
-    /// Progress bars are by default drawn to terminals however if the
-    /// terminal is not user attended the entire progress bar will be
-    /// hidden.  This is done so that piping to a file will not produce
-    /// useless escape codes in that file.
-    ///
-    /// Will panic if refresh_rate is `Some(0)`. To disable rate limiting use `None` instead.
-    pub fn term(term: Term, refresh_rate: impl Into<Option<u64>>) -> ProgressDrawTarget {
-        let rate = refresh_rate.into().map(|x| Duration::from_millis(1000 / x));
-        ProgressDrawTarget {
-            kind: ProgressDrawTargetKind::Term {
-                term,
-                last_state: None,
-                rate,
-                last_draw: None,
-            },
-        }
-    }
-
-    /// A hidden draw target.
-    ///
-    /// This forces a progress bar to be not rendered at all.
-    pub fn hidden() -> ProgressDrawTarget {
-        ProgressDrawTarget {
-            kind: ProgressDrawTargetKind::Hidden,
-        }
-    }
-
-    /// Returns true if the draw target is hidden.
-    ///
-    /// This is internally used in progress bars to figure out if overhead
-    /// from drawing can be prevented.
-    pub fn is_hidden(&self) -> bool {
-        match self.kind {
-            ProgressDrawTargetKind::Hidden => true,
-            ProgressDrawTargetKind::Term { ref term, .. } => !term.is_term(),
-            _ => false,
-        }
-    }
-
-    /// Returns the current width of the draw target.
-    fn width(&self) -> usize {
-        match self.kind {
-            ProgressDrawTargetKind::Term { ref term, .. } => term.size().1 as usize,
-            ProgressDrawTargetKind::Remote { ref state, .. } => state.read().unwrap().width(),
-            ProgressDrawTargetKind::Hidden => unreachable!(),
-        }
-    }
-
-    /// Apply the given draw state (draws it).
-    pub(crate) fn apply_draw_state(&mut self, draw_state: ProgressDrawState) -> io::Result<()> {
-        // no need to apply anything to hidden draw targets.
-        if self.is_hidden() {
-            return Ok(());
-        }
-        match self.kind {
-            ProgressDrawTargetKind::Term {
-                ref term,
-                ref mut last_state,
-                rate,
-                ref mut last_draw,
-            } => {
-                if draw_state.finished
-                    || draw_state.force_draw
-                    || rate.is_none()
-                    || last_draw.is_none()
-                    || last_draw.unwrap().elapsed() > rate.unwrap()
-                {
-                    if let Some(ref last_state) = *last_state {
-                        if !draw_state.lines.is_empty() && draw_state.move_cursor {
-                            last_state.move_cursor(term)?;
-                        } else {
-                            last_state.clear_term(term)?;
-                        }
-                    }
-                    draw_state.draw_to_term(term)?;
-                    term.flush()?;
-                    *last_state = Some(draw_state);
-                    *last_draw = Some(Instant::now());
-                }
-            }
-            ProgressDrawTargetKind::Remote { idx, ref chan, .. } => {
-                return chan
-                    .lock()
-                    .unwrap()
-                    .send((idx, draw_state))
-                    .map_err(|e| io::Error::new(io::ErrorKind::Other, e));
-            }
-            ProgressDrawTargetKind::Hidden => {}
-        }
-        Ok(())
-    }
-
-    /// Properly disconnects from the draw target
-    pub(crate) fn disconnect(&self) {
-        match self.kind {
-            ProgressDrawTargetKind::Term { .. } => {}
-            ProgressDrawTargetKind::Remote { idx, ref chan, .. } => {
-                chan.lock()
-                    .unwrap()
-                    .send((
-                        idx,
-                        ProgressDrawState {
-                            lines: vec![],
-                            orphan_lines: 0,
-                            finished: true,
-                            force_draw: false,
-                            move_cursor: false,
-                        },
-                    ))
-                    .ok();
-            }
-            ProgressDrawTargetKind::Hidden => {}
-        };
-    }
-}
-
-impl ProgressDrawState {
-    pub fn clear_term(&self, term: &Term) -> io::Result<()> {
-        term.clear_last_lines(self.lines.len() - self.orphan_lines)
-    }
-
-    pub fn move_cursor(&self, term: &Term) -> io::Result<()> {
-        term.move_cursor_up(self.lines.len() - self.orphan_lines)
-    }
-
-    pub fn draw_to_term(&self, term: &Term) -> io::Result<()> {
-        for line in &self.lines {
-            term.write_line(line)?;
-        }
-        Ok(())
-    }
-}
-
 /// The state of a progress bar at a moment in time.
 pub(crate) struct ProgressState {
     pub(crate) style: ProgressStyle,
@@ -512,11 +264,6 @@ impl Drop for ProgressState {
     }
 }
 
-pub(crate) struct MultiObject {
-    pub(crate) done: bool,
-    pub(crate) draw_state: Option<ProgressDrawState>,
-}
-
 pub(crate) struct MultiProgressState {
     pub(crate) objects: Vec<MultiObject>,
     pub(crate) ordering: Vec<usize>,
@@ -540,4 +287,256 @@ impl MultiProgressState {
         }
         true
     }
+}
+
+pub(crate) struct MultiObject {
+    pub(crate) done: bool,
+    pub(crate) draw_state: Option<ProgressDrawState>,
+}
+
+/// The drawn state of an element.
+#[derive(Clone, Debug)]
+pub(crate) struct ProgressDrawState {
+    /// The lines to print (can contain ANSI codes)
+    pub lines: Vec<String>,
+    /// The number of lines that shouldn't be reaped by the next tick.
+    pub orphan_lines: usize,
+    /// True if the bar no longer needs drawing.
+    pub finished: bool,
+    /// True if drawing should be forced.
+    pub force_draw: bool,
+    /// True if we should move the cursor up when possible instead of clearing lines.
+    pub move_cursor: bool,
+}
+
+impl ProgressDrawState {
+    pub fn clear_term(&self, term: &Term) -> io::Result<()> {
+        term.clear_last_lines(self.lines.len() - self.orphan_lines)
+    }
+
+    pub fn move_cursor(&self, term: &Term) -> io::Result<()> {
+        term.move_cursor_up(self.lines.len() - self.orphan_lines)
+    }
+
+    pub fn draw_to_term(&self, term: &Term) -> io::Result<()> {
+        for line in &self.lines {
+            term.write_line(line)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub(crate) enum Status {
+    InProgress,
+    DoneVisible,
+    DoneHidden,
+}
+
+/// Target for draw operations
+///
+/// This tells a progress bar or a multi progress object where to paint to.
+/// The draw target is a stateful wrapper over a drawing destination and
+/// internally optimizes how often the state is painted to the output
+/// device.
+pub struct ProgressDrawTarget {
+    pub(crate) kind: ProgressDrawTargetKind,
+}
+
+impl ProgressDrawTarget {
+    /// Draw to a buffered stdout terminal at a max of 15 times a second.
+    ///
+    /// For more information see `ProgressDrawTarget::to_term`.
+    pub fn stdout() -> ProgressDrawTarget {
+        ProgressDrawTarget::term(Term::buffered_stdout(), 15)
+    }
+
+    /// Draw to a buffered stderr terminal at a max of 15 times a second.
+    ///
+    /// This is the default draw target for progress bars.  For more
+    /// information see `ProgressDrawTarget::to_term`.
+    pub fn stderr() -> ProgressDrawTarget {
+        ProgressDrawTarget::term(Term::buffered_stderr(), 15)
+    }
+
+    /// Draw to a buffered stdout terminal at a max of `refresh_rate` times a second.
+    ///
+    /// For more information see `ProgressDrawTarget::to_term`.
+    pub fn stdout_with_hz(refresh_rate: u64) -> ProgressDrawTarget {
+        ProgressDrawTarget::term(Term::buffered_stdout(), refresh_rate)
+    }
+
+    /// Draw to a buffered stderr terminal at a max of `refresh_rate` times a second.
+    ///
+    /// For more information see `ProgressDrawTarget::to_term`.
+    pub fn stderr_with_hz(refresh_rate: u64) -> ProgressDrawTarget {
+        ProgressDrawTarget::term(Term::buffered_stderr(), refresh_rate)
+    }
+
+    /// Draw to a buffered stdout terminal without max framerate.
+    ///
+    /// This is useful when data is known to come in very slowly and
+    /// not rendering some updates would be a problem (for instance
+    /// when messages are used extensively).
+    ///
+    /// For more information see `ProgressDrawTarget::to_term`.
+    pub fn stdout_nohz() -> ProgressDrawTarget {
+        ProgressDrawTarget::term(Term::buffered_stdout(), None)
+    }
+
+    /// Draw to a buffered stderr terminal without max framerate.
+    ///
+    /// This is useful when data is known to come in very slowly and
+    /// not rendering some updates would be a problem (for instance
+    /// when messages are used extensively).
+    ///
+    /// For more information see `ProgressDrawTarget::to_term`.
+    pub fn stderr_nohz() -> ProgressDrawTarget {
+        ProgressDrawTarget::term(Term::buffered_stderr(), None)
+    }
+
+    /// Draw to a terminal, optionally with a specific refresh rate.
+    ///
+    /// Progress bars are by default drawn to terminals however if the
+    /// terminal is not user attended the entire progress bar will be
+    /// hidden.  This is done so that piping to a file will not produce
+    /// useless escape codes in that file.
+    ///
+    /// Will panic if refresh_rate is `Some(0)`. To disable rate limiting use `None` instead.
+    #[allow(clippy::wrong_self_convention)]
+    #[deprecated(since = "0.16.0", note = "Use `ProgressDrawTarget::term` instead")]
+    pub fn to_term(term: Term, refresh_rate: impl Into<Option<u64>>) -> ProgressDrawTarget {
+        ProgressDrawTarget::term(term, refresh_rate)
+    }
+
+    /// Draw to a terminal, optionally with a specific refresh rate.
+    ///
+    /// Progress bars are by default drawn to terminals however if the
+    /// terminal is not user attended the entire progress bar will be
+    /// hidden.  This is done so that piping to a file will not produce
+    /// useless escape codes in that file.
+    ///
+    /// Will panic if refresh_rate is `Some(0)`. To disable rate limiting use `None` instead.
+    pub fn term(term: Term, refresh_rate: impl Into<Option<u64>>) -> ProgressDrawTarget {
+        let rate = refresh_rate.into().map(|x| Duration::from_millis(1000 / x));
+        ProgressDrawTarget {
+            kind: ProgressDrawTargetKind::Term {
+                term,
+                last_state: None,
+                rate,
+                last_draw: None,
+            },
+        }
+    }
+
+    /// A hidden draw target.
+    ///
+    /// This forces a progress bar to be not rendered at all.
+    pub fn hidden() -> ProgressDrawTarget {
+        ProgressDrawTarget {
+            kind: ProgressDrawTargetKind::Hidden,
+        }
+    }
+
+    /// Returns true if the draw target is hidden.
+    ///
+    /// This is internally used in progress bars to figure out if overhead
+    /// from drawing can be prevented.
+    pub fn is_hidden(&self) -> bool {
+        match self.kind {
+            ProgressDrawTargetKind::Hidden => true,
+            ProgressDrawTargetKind::Term { ref term, .. } => !term.is_term(),
+            _ => false,
+        }
+    }
+
+    /// Returns the current width of the draw target.
+    fn width(&self) -> usize {
+        match self.kind {
+            ProgressDrawTargetKind::Term { ref term, .. } => term.size().1 as usize,
+            ProgressDrawTargetKind::Remote { ref state, .. } => state.read().unwrap().width(),
+            ProgressDrawTargetKind::Hidden => unreachable!(),
+        }
+    }
+
+    /// Apply the given draw state (draws it).
+    pub(crate) fn apply_draw_state(&mut self, draw_state: ProgressDrawState) -> io::Result<()> {
+        // no need to apply anything to hidden draw targets.
+        if self.is_hidden() {
+            return Ok(());
+        }
+        match self.kind {
+            ProgressDrawTargetKind::Term {
+                ref term,
+                ref mut last_state,
+                rate,
+                ref mut last_draw,
+            } => {
+                if draw_state.finished
+                    || draw_state.force_draw
+                    || rate.is_none()
+                    || last_draw.is_none()
+                    || last_draw.unwrap().elapsed() > rate.unwrap()
+                {
+                    if let Some(ref last_state) = *last_state {
+                        if !draw_state.lines.is_empty() && draw_state.move_cursor {
+                            last_state.move_cursor(term)?;
+                        } else {
+                            last_state.clear_term(term)?;
+                        }
+                    }
+                    draw_state.draw_to_term(term)?;
+                    term.flush()?;
+                    *last_state = Some(draw_state);
+                    *last_draw = Some(Instant::now());
+                }
+            }
+            ProgressDrawTargetKind::Remote { idx, ref chan, .. } => {
+                return chan
+                    .lock()
+                    .unwrap()
+                    .send((idx, draw_state))
+                    .map_err(|e| io::Error::new(io::ErrorKind::Other, e));
+            }
+            ProgressDrawTargetKind::Hidden => {}
+        }
+        Ok(())
+    }
+
+    /// Properly disconnects from the draw target
+    pub(crate) fn disconnect(&self) {
+        match self.kind {
+            ProgressDrawTargetKind::Term { .. } => {}
+            ProgressDrawTargetKind::Remote { idx, ref chan, .. } => {
+                chan.lock()
+                    .unwrap()
+                    .send((
+                        idx,
+                        ProgressDrawState {
+                            lines: vec![],
+                            orphan_lines: 0,
+                            finished: true,
+                            force_draw: false,
+                            move_cursor: false,
+                        },
+                    ))
+                    .ok();
+            }
+            ProgressDrawTargetKind::Hidden => {}
+        };
+    }
+}
+pub(crate) enum ProgressDrawTargetKind {
+    Term {
+        term: Term,
+        last_state: Option<ProgressDrawState>,
+        rate: Option<Duration>,
+        last_draw: Option<Instant>,
+    },
+    Remote {
+        state: Arc<RwLock<MultiProgressState>>,
+        idx: usize,
+        chan: Mutex<Sender<(usize, ProgressDrawState)>>,
+    },
+    Hidden,
 }
