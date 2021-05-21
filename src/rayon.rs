3f49b0e9ecd56397401f0718835a444273154ef6
mibac138 <5672750+mibac138@users.noreply.github.com>
2021-05-21 12:45:53 +0200

    ProgressFolder shouldn't finish progress bars on completion
    ProgressFolders represent only a part of the whole iterator and
    so shouldn't finish progress bars as they don't have knowledge
    of the completion of the rest of folders and otherwise could
    prematurely finish a progress bar

--- src/rayon.rs
+++ src/rayon.rs
@@ -180,9 +180,6 @@ impl<T, C: Folder<T>> Folder<T> for ProgressFolder<C> {
     }
 
     fn complete(self) -> C::Result {
-        if !self.progress.is_finished() {
-            self.progress.finish_using_style();
-        }
         self.base.complete()
     }
 
