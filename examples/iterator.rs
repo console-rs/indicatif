c9425b6fa67a257486ef4b2dd94f0fd00557e38b
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-09-19 21:09:29 +0200

    Add sleep in iterator example to see bars

--- examples/iterator.rs
+++ examples/iterator.rs
@@ -1,14 +1,19 @@
+use std::thread;
+use std::time::Duration;
+
 use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
 
 fn main() {
     // Default styling, attempt to use Iterator::size_hint to count input size
     for _ in (0..1000).progress() {
         // ...
+        thread::sleep(Duration::from_millis(5));
     }
 
     // Provide explicit number of elements in iterator
     for _ in (0..1000).progress_count(1000) {
         // ...
+        thread::sleep(Duration::from_millis(5));
     }
 
     // Provide a custom bar style
@@ -18,5 +23,6 @@ fn main() {
     ));
     for _ in (0..1000).progress_with(pb) {
         // ...
+        thread::sleep(Duration::from_millis(5));
     }
 }
