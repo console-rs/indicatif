1ac5b0678a192b05dd28a692a0b23e6effe393b4
Chris Laplante <chris.laplante@agilent.com>
2021-12-15 12:02:10 -0500

    modify multi.rs example to use insert_after

--- examples/multi.rs
+++ examples/multi.rs
@@ -11,6 +11,13 @@ fn main() {
 
     let pb = m.add(ProgressBar::new(128));
     pb.set_style(sty.clone());
+
+    let pb2 = m.insert_after(&pb, ProgressBar::new(128));
+    pb2.set_style(sty.clone());
+
+    let pb3 = m.insert_after(&pb2, ProgressBar::new(1024));
+    pb3.set_style(sty);
+
     let h1 = thread::spawn(move || {
         for i in 0..128 {
             pb.set_message(format!("item #{}", i + 1));
@@ -20,29 +27,25 @@ fn main() {
         pb.finish_with_message("done");
     });
 
-    let pb = m.add(ProgressBar::new(128));
-    pb.set_style(sty.clone());
     let h2 = thread::spawn(move || {
         for _ in 0..3 {
-            pb.set_position(0);
+            pb2.set_position(0);
             for i in 0..128 {
-                pb.set_message(format!("item #{}", i + 1));
-                pb.inc(1);
+                pb2.set_message(format!("item #{}", i + 1));
+                pb2.inc(1);
                 thread::sleep(Duration::from_millis(8));
             }
         }
-        pb.finish_with_message("done");
+        pb2.finish_with_message("done");
     });
 
-    let pb = m.add(ProgressBar::new(1024));
-    pb.set_style(sty);
     let _ = thread::spawn(move || {
         for i in 0..1024 {
-            pb.set_message(format!("item #{}", i + 1));
-            pb.inc(1);
+            pb3.set_message(format!("item #{}", i + 1));
+            pb3.inc(1);
             thread::sleep(Duration::from_millis(2));
         }
-        pb.finish_with_message("done");
+        pb3.finish_with_message("done");
     });
 
     let _ = h1.join();
