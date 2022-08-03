ba4df33dd522dee77cf9e7bd83761c99a1c950cc
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2022-08-03 12:15:54 -0400

    move the space before the done msg to the template

--- examples/single.rs
+++ examples/single.rs
@@ -8,25 +8,25 @@ fn main() {
     let pb = ProgressBar::new(1024);
     pb.set_style(
         ProgressStyle::default_bar()
-            .template("[{elapsed_precise}] [{wide_bar} {percent}%{msg}] ({per_sec})")
+            .template("[{elapsed_precise}] [{wide_bar} {percent}% {msg}] ({per_sec})")
             .unwrap(),
     );
     for _ in 0..1024 {
         pb.inc(1);
         thread::sleep(Duration::from_millis(5));
     }
-    pb.finish_with_message(" done");
+    pb.finish_with_message("done");
 
     // create a progressbar that refreshes once per second (every 1000 ms)
     let pb_1hz = ProgressBar::with_draw_target(Some(1024), ProgressDrawTarget::stderr_with_hz(1));
     pb_1hz.set_style(
         ProgressStyle::default_bar()
-            .template("[{elapsed_precise}] [{wide_bar} {percent}%{msg}] ({per_sec})")
+            .template("[{elapsed_precise}] [{wide_bar} {percent}% {msg}] ({per_sec})")
             .unwrap(),
     );
     for _ in 0..1024 {
         pb_1hz.inc(1);
         thread::sleep(Duration::from_millis(5));
     }
-    pb_1hz.finish_with_message(" done");
+    pb_1hz.finish_with_message("done");
 }
